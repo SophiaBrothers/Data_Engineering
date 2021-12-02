f6531a04cbdd8894c35b03838c64956adfd25fca
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-12-02 13:15:13 -0600

    Initial commit

---
+++ Web_Scrapping_LI.py
@@ -0,0 +1,16 @@
+import os;
+
+# these give error: "ImportError: No module named requests)"
+import request ; 
+from bs4 import BeautifulSoup;
+
+# How to include third party Python packages in Sublime Text plugins
+# https://stackoverflow.com/questions/15180537/how-to-include-third-party-python-packages-in-sublime-text-2-plugins
+
+
+
+os.getcwd()
+
+
+
+
